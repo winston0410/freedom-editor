550e28cdd50dcad1209abbebd9dcb393a9a989ac
kghugo <howtostudycantonese@gmail.com>
2020-07-29 21:23:00 +0800

    :white_check_mark: Setting up test with Mocha

---
+++ test/index.spec.js
@@ -0,0 +1,18 @@
+
+describe('FreedomEditorInstance.init()', function () {
+  beforeEach(function () {
+
+  })
+
+  it('should throw if defaultBlock is not defined', function () {
+
+  })
+
+  it('should throw if defaultBlock is not registered', function () {
+
+  })
+
+  it('should throw if non-array is passed to blockTemplate', function () {
+
+  })
+})
