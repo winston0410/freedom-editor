e7ab228912be555ca76116619d26b6e61d790ea8
kghugo <howtostudycantonese@gmail.com>
2020-07-29 22:06:35 +0800

    :heavy_minus_sign: Replace jsdom with basichtml

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,18 +1,30 @@
+const {
+  FreedomEditor
+} = require('../src/index.js')
+
+const { document } = require('basichtml').init()
+
+const chai = require('chai')
+const expect = chai.expect
 
 describe('FreedomEditorInstance.init()', function () {
   beforeEach(function () {
+    const editor = new FreedomEditor(
+      {
 
+      }
+    )
   })
 
   it('should throw if defaultBlock is not defined', function () {
-
+    expect(editor.init()).to.throw()
   })
 
-  it('should throw if defaultBlock is not registered', function () {
+  /* it('should throw if defaultBlock is not registered', function () {
 
   })
 
   it('should throw if non-array is passed to blockTemplate', function () {
 
-  })
+  }) */
 })
