ec67d32bc814e7d823dfd719281a2b6da11d20e8
kghugo <howtostudycantonese@gmail.com>
2020-08-01 22:13:50 +0800

    :white_check_mark: Add test for renderBlock()

---
+++ test/renderBlock.spec.js
@@ -0,0 +1,101 @@
+const {
+  FreedomEditor
+} = require('../src/index.js')
+
+const {
+  Paragraph
+} = require('@freedom-editor/vanilla-paragraph-block')
+
+const jsdom = require('jsdom')
+const {
+  JSDOM
+} = jsdom
+const dom = new JSDOM('<html><body></body></html>', {
+  pretendToBeVisual: true
+})
+
+global.document = dom.window.document
+global.window = dom.window
+global.navigator = dom.window.navigator
+
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+
+describe('FreedomEditorInstance.renderBlock', function () {
+  let editorContainer, editor, paragraphBlock
+
+  beforeEach(function (done) {
+    paragraphBlock = new Paragraph()
+    editorContainer = document.createElement('div')
+    editorContainer.setAttribute('id', 'freedom-editor')
+    document.body.append(editorContainer)
+
+    editor = new FreedomEditor({
+      containerId: 'freedom-editor',
+      defaultBlock: [
+        paragraphBlock
+      ],
+      registeredBlocks: [
+        paragraphBlock
+      ],
+      blockTemplate: [
+
+      ]
+    })
+
+    editor.init([])
+
+    editor.loadBlocks()
+
+    done()
+  })
+
+  it('should return the rendered element', function (done) {
+    editor.options.registeredBlocks.forEach((blockInstance) => {
+      const renderedBlock = editor.renderBlock(blockInstance)
+      expect(renderedBlock).to.match(`[data-block-type="${blockInstance.constructor.name}"]`)
+      expect(editor.editor).to.contain(renderedBlock)
+    })
+
+    done()
+  })
+
+  describe('if the block to render is listed in block template', function () {
+    it('should set data-block-template as true', function (done) {
+      editor.options.registeredBlocks.forEach((blockInstance) => {
+        const renderedBlock = editor.renderBlock(blockInstance, 'true')
+        expect(renderedBlock).to.match('[data-block-template="true"]')
+      })
+
+      done()
+    })
+  })
+
+  describe('if the block to render is not listed in block template', function () {
+    it('should set data-block-template as false', function (done) {
+      editor.options.registeredBlocks.forEach((blockInstance) => {
+        const renderedBlock = editor.renderBlock(blockInstance, 'false')
+        expect(renderedBlock).to.match('[data-block-template="false"]')
+      })
+      done()
+    })
+  })
+
+  describe('if pass block data to it', function () {
+    it('should load block data', function (done) {
+      const data = {
+        type: paragraphBlock.constructor.name,
+        data: {
+          text: 'Testing'
+        }
+      }
+
+      const renderedBlock = editor.renderBlock(editor.options.registeredBlocks[0], 'false', data)
+
+      expect(renderedBlock.querySelector('[contenteditable]')).to.have.text(data.data.text)
+
+      done()
+    })
+  })
+})
