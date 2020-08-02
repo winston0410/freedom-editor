e6ef6a45ad7db0f75b15ce385d2c9020be91c837
kghugo <howtostudycantonese@gmail.com>
2020-08-02 13:43:12 +0800

    :sparkles: Create a helper function to check for required parameter

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -129,6 +129,14 @@ const getBlockInstancesListFromDOM = (editorInstance) => {
     })
 }
 
+const requiredParam = (param) => {
+  const requiredParamError = new Error(`Required parameter, "${param}" is missing.`)
+  if (typeof Error.captureStackTrace === 'function') {
+    Error.captureStackTrace(requiredParamError, requiredParam)
+  }
+  throw requiredParamError
+}
+
 module.exports = {
   shiftBlockFocus,
   shiftFieldFocus,
